e87e6bcf26ecd6e1bca0577587d4eaae62c25252
jeswanthv <jeswanthv01@gmail.com>
2023-11-26 18:09:51 -0800

    Added pages

---
+++ src/pages/drive/[...Folder].tsx
@@ -0,0 +1,100 @@
+import React from "react";
+import { useRouter } from "next/router";
+import GetFolders from "@/components/GetFolders";
+import GetFiles from "@/components/GetFiles";
+import Head from "next/head";
+import FileHeader from "@/components/FileHeader";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { fetchFiles } from "@/hooks/fetchFiles";
+import { DotLoader } from "react-spinners";
+
+function Folder() {
+  const [isFolder, setIsFolder] = useState(false);
+  const [isFile, setIsFile] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const router = useRouter();
+  const { Folder } = router.query;
+
+  const { data: session } = useSession();
+
+  // Fetch the list of files and folders
+  const list = fetchFiles(Folder?.[1] || "", session?.user.email!);
+
+  useEffect(() => {
+    // Determine if there are folders and files in the list
+    const hasFolders = list.some((item) => item.isFolder);
+    const hasFiles = list.some((item) => !item.isFolder);
+
+    // Update the state based on the results
+    setIsFolder(hasFolders);
+    setIsFile(hasFiles);
+
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2200);
+  }, [list]);
+
+  return (
+    <>
+      <Head>
+        <title>Guardian</title>
+        <meta name="description" content="Secured File Sharing Platform" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <FileHeader headerName={"Nested Folder"} />
+        <div className="h-[75vh] w-full overflow-y-auto p-5">
+          {/* If the list is loading, display the loading state */}
+          {!isFile && !isFolder && isLoading ? (
+            <div className="flex h-full items-center justify-center">
+              <DotLoader color="#b8c2d7" size={60} />
+            </div>
+          ) : (
+            <>
+              {/* If there are files or folders, display them */}
+              {isFile || isFolder ? (
+                <>
+                  {isFolder && (
+                    // If there are folders, display them
+                    <div className="mb-5 flex flex-col space-y-4">
+                      <h2>Folders</h2>
+                      <div className="flex flex-wrap justify-start gap-x-3 gap-y-5 text-textC">
+                        <GetFolders folderId={Folder?.[1] || ""} select={""} />
+                      </div>
+                    </div>
+                  )}
+                  {isFile && (
+                    // If there are files, display them
+                    <div className="mb-5 flex flex-col space-y-4">
+                      <h2>Files</h2>
+                      <div className="flex flex-wrap justify-start gap-x-3 gap-y-5 text-textC">
+                        <GetFiles folderId={Folder?.[1] || ""} select={""} type="default" />
+                      </div>
+                    </div>
+                  )}
+                </>
+              ) : (
+                // If there are no files or folders, display the empty state
+                <div className="flex h-full flex-col items-center justify-center">
+                  <Image
+                    draggable={false}
+                    src="/empty_state_folder.png"
+                    width={500}
+                    height={500}
+                    alt="empty-state"
+                    className="w-full max-w-md object-cover object-center opacity-75"
+                  />
+                </div>
+              )}
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Folder;
---
+++ src/pages/drive/my-drive/index.tsx
@@ -0,0 +1,96 @@
+import Head from "next/head";
+import GetFiles from "@/components/GetFiles";
+import GetFolders from "@/components/GetFolders";
+import FileHeader from "@/components/FileHeader";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { fetchFiles } from "@/hooks/fetchFiles";
+import { DotLoader } from "react-spinners";
+
+export default function Home() {
+  const [isFolder, setIsFolder] = useState(false);
+  const [isFile, setIsFile] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const { data: session } = useSession();
+
+  // Fetch the list of files and folders
+  const list = fetchFiles("", session?.user.email!);
+
+  useEffect(() => {
+    // Determine if there are folders and files in the list
+    const hasFolders = list.some((item) => item.isFolder && !item.isTrashed);
+    const hasFiles = list.some((item) => !item.isFolder && !item.isTrashed);
+
+    // Update the state based on the results
+    setIsFolder(hasFolders);
+    setIsFile(hasFiles);
+
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2200);
+  }, [list]);
+
+  return (
+    <>
+      <Head>
+        <title>Guardian</title>
+        <meta name="description" content="Secured File Sharing Platform" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <FileHeader headerName={"My Drive"} />
+        <div className="h-[75vh] w-full overflow-y-auto p-5">
+          {/* If the list is loading, display the loading state */}
+          {!isFile && !isFolder && isLoading ? (
+            <div className="flex h-full items-center justify-center">
+              <DotLoader color="#b8c2d7" size={60} />
+            </div>
+          ) : (
+            <>
+              {/* If there are files or folders, display them */}
+              {isFile || isFolder ? (
+                <>
+                  {isFolder && (
+                    // If there are folders, display them
+                    <div className="mb-5 flex flex-col space-y-4">
+                      <h2>Folders</h2>
+                      <div className="flex flex-wrap justify-start gap-x-3 gap-y-5 text-textC">
+                        <GetFolders folderId="" select="" />
+                      </div>
+                    </div>
+                  )}
+                  {isFile && (
+                    // If there are files, display them
+                    <div className="mb-5 flex flex-col space-y-4">
+                      <h2>Files</h2>
+                      <div className="flex flex-wrap justify-start gap-x-3 gap-y-5 text-textC">
+                        <GetFiles folderId="" select="" type="default" />
+                      </div>
+                    </div>
+                  )}
+                </>
+              ) : (
+                // If there are no files or folders, display the empty state
+                <div className="flex h-full flex-col items-center justify-center">
+                  <h2 className="mb-5 text-xl font-medium text-textC">
+                    A place for all of your files
+                  </h2>
+                  <Image
+                    draggable={false}
+                    src="/empty_state_drive.png"
+                    width={500}
+                    height={500}
+                    alt="empty-state"
+                    className="w-full max-w-2xl object-cover object-center"
+                  />
+                </div>
+              )}
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
